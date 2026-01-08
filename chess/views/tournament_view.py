758d440b896546a036243849286aaaf7ade66a77
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-01-08 17:46:32 +0100

    fonction inutile delete

--- chess/views/tournament_view.py
+++ chess/views/tournament_view.py
@@ -59,8 +59,3 @@ class Tournament_views:
     def ask_next_round() -> str:
         choice = rich.input("[yellow]Lancer le round suivant (oui/non): [/]")
         return choice
-
-    # Affiche un tournoi de la base de données
-    @staticmethod
-    def get_start_tournament():
-        pass
